1b5c282d2c804e9057b056baad86555d96498d01
BhargavJhaveri <bhargavjhaveri@gmail.com>
2016-03-30 01:28:27 +0000

    Client code RPi

--- RPi/RPi_subs.py
+++ RPi/RPi_subs.py
@@ -6,7 +6,9 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+#    client.subscribe("$SYS/#")
+    client.subscribe("sensor/temp")
+    
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -16,7 +18,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("iot.eclipse.org", 1883, 60)
+client.connect("127.0.0.1", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
